68084c88406409b642c5be75ede0165bf9284a52
putratrieka <putratrieka@gmail.com>
2019-07-16 21:31:24 +0700

    splash screen

---
+++ app/src/main/java/com/trieka/latihanandroid/SplashActivity.java
@@ -0,0 +1,45 @@
+package com.trieka.latihanandroid;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashActivity extends AppCompatActivity {
+    private Context context = this;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //FULLSCREEN CODE
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_main);
+
+        // TIMER
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+
+                pindahKeLoginScreen();
+            }
+        };
+
+        Timer timer = new Timer();
+        timer.schedule(timerTask, 3000);
+    }
+    private void pindahKeLoginScreen(){
+        Intent intent = new Intent(context,
+                LoginActivity.class);
+        startActivity(intent);
+
+        finish();
+    }
+}

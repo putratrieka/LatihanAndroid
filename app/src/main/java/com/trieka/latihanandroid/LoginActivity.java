32d5646bc313f13c163d67d3ae509866691912aa
putratrieka <putratrieka@gmail.com>
2019-07-16 22:06:52 +0700

    Login Screen

--- app/src/main/java/com/trieka/latihanandroid/LoginActivity.java
+++ app/src/main/java/com/trieka/latihanandroid/LoginActivity.java
@@ -1,13 +1,71 @@
 package com.trieka.latihanandroid;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.trieka.latihanandroid.utility.Constanta;
 
 public class LoginActivity extends AppCompatActivity {
+
+    // DECLARE VARIABLE
+    private Context context = this;
+    private int counter_back = 1;
+
+    private EditText inputUsername, inputPassword;
+    private CheckBox checkBoxRemember;
+    private Button buttonLogin;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //INISIALISASI
+        inputUsername = (EditText) findViewById(R.id.inputUsername);
+        inputPassword = (EditText) findViewById(R.id.inputPassword);
+        checkBoxRemember = (CheckBox) findViewById(R.id.checkBoxRemember);
+
+        buttonLogin = (Button) findViewById(R.id.buttonLogin);
+        //tambahkan action listener
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //deklarasikan aksinya
+                validasiInput();
+            }
+        });
+    }
+    // VALIDASI INPUT
+    private void validasiInput() {
+        String valueUsername = inputUsername.getText().toString().trim();
+        String valuePassword = inputPassword.getText().toString().trim();
+
+        if (valueUsername.equalsIgnoreCase("")) {
+            Toast.makeText(context, "Username masih kosong!", Toast.LENGTH_SHORT).show();
+        } else if (valuePassword.length() == 0) {
+            Toast.makeText(context, "Password masih kosong!", Toast.LENGTH_SHORT).show();
+        } else {
+            //validasi sukses
+//                boolean remember = checkBoxRemember.isChecked();
+//
+//                // simpan data login
+//                SessionManager.simpanDataLogin(context, valueUsername, valuePassword, remember);
+
+            Intent pindah = new Intent(context, HomeActivity.class);
+            pindah.putExtra(Constanta.ID_EXTRA_USERNAME, valueUsername);
+            pindah.putExtra(Constanta.ID_EXTRA_PASSWORD, valuePassword);
+
+            startActivity(pindah);
+
+            finish();
+        }
     }
 }
---
+++ app/src/main/java/com/trieka/latihanandroid/utility/Constanta.java
@@ -0,0 +1,7 @@
+package com.trieka.latihanandroid.utility;
+
+public class Constanta {
+    public static String ID_EXTRA_USERNAME = "USERNAME";
+    public static String ID_EXTRA_PASSWORD = "PASSWORD";
+
+}
